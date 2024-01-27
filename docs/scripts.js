f5bfe0ce037bf74b9391d9403549f30dca0a053f
Harrison Cook <HonsonCooky@gmail.com>
2024-01-28 05:13:24 +1300

    add vim and edge shortcuts with searching

--- docs/scripts.js
+++ docs/scripts.js
@@ -72,12 +72,138 @@ document.addEventListener("keydown", (event) => {
 });
 
 // -----------------------------------------------------------------------------------------------------------------
+// # SEARCH BOX FUNCTIONALITY
+// -----------------------------------------------------------------------------------------------------------------
+
+let currentIndex = -1;
+let matches = [];
+let prevQuery = "";
+const mark = "marked";
+const highlight = "marked-highlight";
+
+function scrollIntoViewIfNeeded(element) {
+  element.classList.add(highlight);
+  element.scrollIntoView({ block: "center", inline: "nearest" });
+}
+
+function searchChildren(element, query, depth) {
+  if (depth === 0 && query != prevQuery) {
+    matches = [];
+    currentIndex = -1;
+    prevQuery = query;
+  }
+
+  const parent = element.parentNode;
+  parent.classList.remove(mark);
+  parent.classList.remove(highlight);
+  if (element.nodeType === Node.TEXT_NODE && query.length > 0 && element.nodeValue.includes(query)) {
+    parent.classList.add(mark);
+    matches.push(parent);
+  }
+
+  for (let i = 0; i < element.childNodes.length; i++) {
+    searchChildren(element.childNodes[i], query, depth + 1);
+  }
+}
+
+function findNext() {
+  if (matches.length > 0) {
+    if (currentIndex != -1) matches[currentIndex].classList.remove(highlight);
+    currentIndex = (currentIndex + 1) % matches.length;
+    scrollIntoViewIfNeeded(matches[currentIndex]);
+  }
+}
+
+function findPrevious() {
+  if (matches.length > 0) {
+    if (currentIndex != -1) matches[currentIndex].classList.remove(highlight);
+    currentIndex = (currentIndex - 1 + matches.length) % matches.length;
+    scrollIntoViewIfNeeded(matches[currentIndex]);
+  }
+}
+
+function createSearchBox(searchElement) {
+  const searchBox = document.createElement("input");
+  searchBox.type = "text";
+  searchBox.placeholder = "Search";
+
+  searchBox.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      if (this.value != prevQuery) searchChildren(searchElement, this.value, 0);
+      if (event.shiftKey) {
+        findPrevious();
+      } else {
+        findNext();
+      }
+    }
+  });
+  return searchBox;
+}
+
+// -----------------------------------------------------------------------------------------------------------------
 // # SHORTCUT LOADING
 // -----------------------------------------------------------------------------------------------------------------
-const vimShortcuts = document.getElementById("vim-shortcuts")
+const vimShortcuts = document.getElementById("vim-shortcuts");
+const edgeShortcuts = document.getElementById("edge-shortcuts");
+
+function createSubSection(key, level) {
+  const div = document.createElement("div");
+  div.id = `${key}-${level}`;
+  div.className = "sub-section";
+
+  const header = document.createElement(`h${level + 2}`);
+  header.innerText = key;
+  if (level != 0) {
+    div.appendChild(header);
+  } else {
+    const searchBox = createSearchBox(div);
+    const headerDiv = document.createElement("div");
+    headerDiv.className = "header-container";
+    headerDiv.appendChild(header);
+    headerDiv.appendChild(searchBox);
+    div.appendChild(headerDiv);
+  }
+
+  return div;
+}
+
+function createListElement(key, value, level) {
+  const div = document.createElement("div");
+  div.id = `${key}-${level}`;
+  div.className = "list-element";
+
+  const keyTxt = document.createElement("span");
+  keyTxt.innerText = key;
+  const valueTxt = document.createElement("span");
+  valueTxt.innerText = value;
+
+  div.appendChild(keyTxt);
+  div.appendChild(valueTxt);
+
+  return div;
+}
+
+function createShortcut(key, value, level) {
+  const section = createSubSection(key, level);
+
+  Object.entries(value)
+    .map(([key, value]) => {
+      const nextLevel = level + 1;
+      if (value != null && typeof value === "object") return createShortcut(key, value, nextLevel);
+      return createListElement(key, value, nextLevel);
+    })
+    .forEach((child) => section.appendChild(child));
+  return section;
+}
+
 fetch("./assets/vim-shortcuts.json")
   .then((response) => response.json())
-  .then((data) => );
+  .then((data) => vimShortcuts.appendChild(createShortcut("Vim Shortcuts", data, 0)));
+
+fetch("./assets/edge-shortcuts.json")
+  .then((response) => response.json())
+  .then((data) => edgeShortcuts.appendChild(createShortcut("Edge Shortcuts", data, 0)));
 
 // -----------------------------------------------------------------------------------------------------------------
 // # STARTUP
