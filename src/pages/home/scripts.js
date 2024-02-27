dfda737708ad91f42c9d17ef72ff01656d268acb
Harrison Cook <HonsonCooky@gmail.com>
2024-02-27 13:26:04 +1300

    create home page

--- src/pages/home/scripts.js
+++ src/pages/home/scripts.js
@@ -1,1 +1,5 @@
 import "./styles.css";
+import logoImg from "../../assets/HC_LOGO_LIGHT_128x128.png";
+
+// LOAD LOGO
+Array.from(document.querySelectorAll(".logo")).forEach((i) => (i.src = logoImg));
