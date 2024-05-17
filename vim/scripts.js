ba554bc7b47d3dc237f50896bc201732adc9b0a4
Harrison Cook <HonsonCooky@gmail.com>
2024-05-17 18:35:22 +1200

    finish shortcuts

--- vim/scripts.js
+++ vim/scripts.js
@@ -1,8 +1,2 @@
 import { loadKeymap } from "../assets/global.js";
-
-(function clock() {
-  document.getElementById("date").innerHTML = new Date().toISOString();
-  setTimeout(clock, 80);
-})();
-
 loadKeymap();
